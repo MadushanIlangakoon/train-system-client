33e19e7fda9c1b8cc07a164d502cc9a09432a6bc
Ilangakoon Mudiyanselage Kavishka Madushan <146872058+MadushanIlangakoon@users.noreply.github.com>
2024-08-01 22:22:41 +0530

    adding project

--- controllers/trainController.js
+++
@@ -1,4 +0,0 @@
-export const getTrainData = async (req, res) => {
-    res.status(200).json({ message: "Train data fetched successfully" });
-  };
-  
--- models/trainModel.js
+++
@@ -1,7 +0,0 @@
-export const fetchTrainData = async () => {
-    return [
-      { id: 1, name: "Train 1", status: "On Time" },
-      { id: 2, name: "Train 2", status: "Delayed" }
-    ];
-  };
-  
